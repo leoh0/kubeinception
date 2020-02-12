c4cfd4dfde08e4a08a73110d334ffae0bbdcac95
Eohyung Lee <liquidnuker@gmail.com>
2020-02-12 10:55:23 +0900

    Some changes

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # kubeinception
 
+[![asciicast](https://asciinema.org/a/300370.svg)](https://asciinema.org/a/300370)
+
 > 당신의 limbo는 몇단계에서 올까요?
 
-과연 kubernetes 안에서 kubernetes는 몇단계 까지 실행될 수 있을까요?
+kubernetes 안에서 kubernetes는 몇단계 까지 실행될 수 있을까요?
 
-아래를 실행시키면 영원히 kubernetes 안에서 kubernetes를 실행하게 됩니다.
+아래를 실행시키면 확인 할 수 있습니다.
 
 ```
 kubectl apply -f create-kind.yaml
 ```
+
+## faq
+
+> Q.  kind 클러스터 생성시 registry에서 이미지를 못땅겨 옵니다.
+
+* 도메인 서버를 1.1.1.1 외의 다른 것으로 바꿔보세요.
+  
--- run.sh
+++ run.sh
@@ -1,57 +1,115 @@
 #!/bin/sh
 
-set -x
+BANNER=kubernetes
+for i in $(seq 1 $DEPTH); do
+    BANNER=$(printf "kubernetes in %s\n" "$BANNER")
+done
 
+echo
+echo -e "\033[00;36m###############################################\033[00m"
+echo -e "\033[00;32m$BANNER\033[00m"
+echo -e "\033[00;31mSTAGE: $DEPTH\033[00m"
+echo -e "\033[00;36m###############################################\033[00m"
+echo
+sleep 3
 echo 
-echo "####### LAUNCH DOCKER DAEMON #######"
+echo -e "\033[00;36m####### LAUNCH DOCKER DAEMON #######\033[00m"
 echo 
-nohup dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 &
+nohup dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 > dockerd.log 2>&1 &
 
 wait=0
 while true; do
     docker ps -q > /dev/null 2>&1 && break
-    if [[ ${wait} -lt 60 ]]; then
+    if [[ ${wait} -lt 10 ]]; then
         wait=$((wait+1))
         echo "waiting for docker ..."
-        sleep 1
+        sleep ${wait}
     else
         echo "docker is broken ..."
         exit 1
     fi
 done
 
-for i in $(seq 1 10); do 
-  echo "waiting $i";
-  sleep 1;
-done;
-
 echo 
-echo "####### CREATE KIND CLUSTER #######"
+echo -e "\033[00;36m####### CREATE KIND CLUSTER #######\033[00m"
 echo 
 kind create cluster
 kind get kubeconfig > file
 
 cat file
 
+# waiting admission controller
 wait=0;
 while true; do
     kubectl --kubeconfig=/kube/file get sa default && break
-    if [[ ${wait} -lt 60 ]]; then
+    if [[ ${wait} -lt 10 ]]; then
         wait=$((wait+1))
         echo "waiting for creating sa ..."
-        sleep 1
+        sleep ${wait}
     else
         echo "sa is not ready ..."
         exit 1
     fi
 done
 
-echo "sleep 10 secs for waiting core component ..."
-sleep 10
+# confirm creating kube-system component
+# From https://github.com/groundnuty/k8s-wait-for/blob/master/wait_for.sh
+wait=0;
+while true; do
+    STATUS=$(kubectl --kubeconfig=/kube/file get pods -n kube-system -o go-template='
+{{- define "checkStatus" -}}
+  {{- $rootStatus := .status }}
+  {{- $hasReadyStatus := false }}
+  {{- range .status.conditions -}}
+    {{- if eq .type "Ready" -}}
+      {{- $hasReadyStatus = true }}
+      {{- if eq .status "False" -}}
+        {{- if .reason -}}
+          {{- if ne .reason "PodCompleted" -}}
+            {{ .status }}
+            {{- range $rootStatus.containerStatuses -}}
+              {{- if .state.terminated.reason -}}
+              :{{ .state.terminated.reason }}
+              {{- end -}}
+            {{- end -}}
+          {{- end -}}
+        {{- else -}}
+          {{ .status }}
+        {{- end -}}
+      {{- end -}}
+    {{- end -}}
+  {{- else -}}
+    {{- printf "No resources found.\n" -}}
+  {{- end -}}
+  {{- if ne $hasReadyStatus true -}}
+    {{- printf "False" -}}
+  {{- end -}}
+{{- end -}}
+{{- if .items -}}
+    {{- range .items -}}
+      {{ template "checkStatus" . }}
+    {{- end -}}
+{{- else -}}
+    {{ template "checkStatus" . }}
+{{- end -}}')
+    if [[ "$STATUS" == "" ]] ; then
+        break
+    fi
+    if [[ ${wait} -lt 10 ]]; then
+        wait=$((wait+1))
+        echo "waiting for kube-system component ..."
+        sleep ${wait}
+    else
+        echo "one of kube-system component is broken ..."
+        exit 1
+    fi
+done
 
 echo 
-echo "####### REBIRTH KIND POD #######"
+echo -e "\033[00;36m####### REBIRTH KIND POD #######\033[00m"
 echo 
+NEWDEPTH=$((DEPTH+1))
+sed -i "s/value:.*$/value: \"$NEWDEPTH\"/g" /kube/create-kind.yaml
 kubectl --kubeconfig=/kube/file apply -f /kube/create-kind.yaml || :
 
 # sometime kind api is not ready for getting pods log
@@ -60,13 +118,65 @@ kubectl --kubeconfig=/kube/file apply -f /kube/create-kind.yaml || :
 echo "sleep 10 secs for waiting pod ..."
 sleep 10
 
+# confirm creating kind pod
 wait=0;
 while true; do
-    kubectl --kubeconfig=/kube/file logs kubeinception && break
-    if [[ ${wait} -lt 120 ]]; then
+    STATUS=$(kubectl --kubeconfig=/kube/file get pods -o go-template='
+{{- define "checkStatus" -}}
+  {{- $rootStatus := .status }}
+  {{- $hasReadyStatus := false }}
+  {{- range .status.conditions -}}
+    {{- if eq .type "Ready" -}}
+      {{- $hasReadyStatus = true }}
+      {{- if eq .status "False" -}}
+        {{- if .reason -}}
+          {{- if ne .reason "PodCompleted" -}}
+            {{ .status }}
+            {{- range $rootStatus.containerStatuses -}}
+              {{- if .state.terminated.reason -}}
+              :{{ .state.terminated.reason }}
+              {{- end -}}
+            {{- end -}}
+          {{- end -}}
+        {{- else -}}
+          {{ .status }}
+        {{- end -}}
+      {{- end -}}
+    {{- end -}}
+  {{- else -}}
+    {{- printf "No resources found.\n" -}}
+  {{- end -}}
+  {{- if ne $hasReadyStatus true -}}
+    {{- printf "False" -}}
+  {{- end -}}
+{{- end -}}
+{{- if .items -}}
+    {{- range .items -}}
+      {{ template "checkStatus" . }}
+    {{- end -}}
+{{- else -}}
+    {{ template "checkStatus" . }}
+{{- end -}}')
+    if [[ "$STATUS" == "" ]] ; then
+        break
+    fi
+    if [[ ${wait} -lt 10 ]]; then
+        wait=$((wait+1))
+        echo "waiting for kind ..."
+        sleep ${wait}
+    else
+        echo "newer kind is broken ..."
+        exit 1
+    fi
+done
+
+wait=0;
+while true; do
+    kubectl --kubeconfig=/kube/file logs kubeinception > /dev/null && break
+    if [[ ${wait} -lt 10 ]]; then
         wait=$((wait+1))
         echo "waiting for creating pod ..."
-        sleep 1
+        sleep ${wait}
     else
         echo "pod is not ready ..."
         exit 1
